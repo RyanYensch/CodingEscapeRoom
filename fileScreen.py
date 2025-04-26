b9adc53e017485b6bc1f18a07199091e81cba131
RyanYensch <ry2005@outlook.com>
2025-04-27 05:18:07 +1000

    created the base template for the file

---
+++ fileScreen.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from windowSetting import setCenter
+
+def openFile(editor, window):
+    editorWindow= editor.openEditor()
+    window.wait_window(editorWindow)
+    
+
+class FileScreen():
+    def __init__(self, text = ""):
+        pass
+    
+    def openFile():
+        pass
+    
+    
