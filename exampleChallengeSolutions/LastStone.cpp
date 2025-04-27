cd0b0255f7cdeee47c6be89b5554af5deca2835e
RyanYensch <ry2005@outlook.com>
2025-04-27 18:38:27 +1000

    added last stone weight challenge

---
+++ exampleChallengeSolutions/LastStone.cpp
@@ -0,0 +1,17 @@
+#include "ALL_H_FILES.h"
+using namespace std;
+
+
+class LastStone {
+public:
+    int lastStoneWeight(vector<int> stones) {
+        priority_queue<int> pq(begin(stones), end(stones));
+        while (pq.size() > 1) {
+            int x = pq.top(); pq.pop();
+            int y = pq.top(); pq.pop();
+            if (x > y) pq.push(x - y);
+        }
+        return pq.empty() ? 0 : pq.top();
+    }
+};
+
